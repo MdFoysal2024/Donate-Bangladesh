0481d0f8aadc5e357b8f6acbf743def27dae5861
MdFoysal2024 <mdfoysal010199@gmail.com>
2024-09-25 07:01:14 +0600

    Routes Activation is done

--- js/donateBtn.js
+++ js/donateBtn.js
@@ -1,6 +1,6 @@
-// section-0----->
+
 document.getElementById('btn-donate').addEventListener('click', function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const addDonation = getInputFieldValueById('input-field-value');
     const accountBalance = getTextValueById('account-balance');
@@ -9,7 +9,7 @@ document.getElementById('btn-donate').addEventListener('click', function (event)
 
     if ((addDonation > 0) && (typeof parseFloat(addDonation) === 'number')) {
 
-        if(parseFloat(addDonation) > parseFloat(accountBalance)){
+        if (parseFloat(addDonation) > parseFloat(accountBalance)) {
             alert('Invalid Donation Amount');
             return;
         }
@@ -18,21 +18,19 @@ document.getElementById('btn-donate').addEventListener('click', function (event)
         document.getElementById('old-donation-balance').innerText = updateDonation;
 
         const newPageBalance = parseFloat(accountBalance) - parseFloat(addDonation);
-        console.log(newPageBalance);
-        document.getElementById('account-balance').innerText = newPageBalance
 
-        const currentTime = new Date()
-        const div = document.createElement('div')
+        document.getElementById('account-balance').innerText = newPageBalance;
+
+        const currentTime = new Date();
+        const div = document.createElement('div');
         div.innerHTML = `
             <div class="border  bg-[#F9F7F3] border-gray-200 rounded-lg p-8 mt-12">
                 <p class=" text-lg font-bold ">${addDonation} Taka is ${floodArea}</p>
                 <p class=" ">${currentTime}</p>
             </div>
             `
-        document.getElementById('history-container').appendChild(div)
-
+        document.getElementById('history-container').appendChild(div);
     }
-
     else {
         alert('Invalid Donation Amount');
     }
@@ -40,9 +38,9 @@ document.getElementById('btn-donate').addEventListener('click', function (event)
 })
 
 
-// section-2----->
+
 document.getElementById('btn-donate-02').addEventListener('click', function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const addDonation = getInputFieldValueById('input-field-value-02');
     const accountBalance = getTextValueById('account-balance');
@@ -51,30 +49,28 @@ document.getElementById('btn-donate-02').addEventListener('click', function (eve
 
     if ((addDonation > 0) && (typeof parseFloat(addDonation) === 'number')) {
 
-        if(parseFloat(addDonation) > parseFloat(accountBalance)){
+        if (parseFloat(addDonation) > parseFloat(accountBalance)) {
             alert('Invalid Donation Amount');
             return;
         }
         const updateDonation = parseFloat(addDonation) + parseFloat(oldDonation);
-        console.log(updateDonation);
+
         document.getElementById('old-donation-balance-02').innerText = updateDonation;
 
         const newPageBalance = parseFloat(accountBalance) - parseFloat(addDonation);
-        console.log(newPageBalance);
-        document.getElementById('account-balance').innerText = newPageBalance
 
-        const currentTime = new Date()
-        const div = document.createElement('div')
+        document.getElementById('account-balance').innerText = newPageBalance;
+
+        const currentTime = new Date();
+        const div = document.createElement('div');
         div.innerHTML = `
             <div class="border  bg-[#F9F7F3] border-gray-200 rounded-lg p-8 mt-12">
                 <p class=" text-lg font-bold ">${addDonation} Taka is ${floodArea}</p>
-                <p class=" ">${currentTime}</p>
+                <p class=" ">Date: ${currentTime}</p>
             </div>
             `
-        document.getElementById('history-container').appendChild(div)
-
+        document.getElementById('history-container').appendChild(div);
     }
-
     else {
         alert('Invalid Donation Amount');
     }
@@ -82,9 +78,9 @@ document.getElementById('btn-donate-02').addEventListener('click', function (eve
 })
 
 
-// section-3----->
+
 document.getElementById('btn-donate-03').addEventListener('click', function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const addDonation = getInputFieldValueById('input-field-value-03');
     const accountBalance = getTextValueById('account-balance');
@@ -93,27 +89,25 @@ document.getElementById('btn-donate-03').addEventListener('click', function (eve
 
     if ((addDonation > 0) && (typeof parseFloat(addDonation) === 'number')) {
 
-        if(parseFloat(addDonation) > parseFloat(accountBalance)){
+        if (parseFloat(addDonation) > parseFloat(accountBalance)) {
             alert('Invalid Donation Amount');
             return;
         }
         const updateDonation = parseFloat(addDonation) + parseFloat(oldDonation);
         document.getElementById('old-donation-balance-03').innerText = updateDonation;
         const newPageBalance = parseFloat(accountBalance) - parseFloat(addDonation);
-        document.getElementById('account-balance').innerText = newPageBalance
+        document.getElementById('account-balance').innerText = newPageBalance;
 
-        const currentTime = new Date()
-        const div = document.createElement('div')
+        const currentTime = new Date();
+        const div = document.createElement('div');
         div.innerHTML = `
             <div class="border  bg-[#F9F7F3] border-gray-200 rounded-lg p-8 mt-12">
                 <p class=" text-lg font-bold ">${addDonation} Taka is ${floodArea}</p>
                 <p class=" ">${currentTime}</p>
             </div>
             `
-        document.getElementById('history-container').appendChild(div)
-
+        document.getElementById('history-container').appendChild(div);
     }
-
     else {
         alert('Invalid Donation Amount');
     }
@@ -121,9 +115,10 @@ document.getElementById('btn-donate-03').addEventListener('click', function (eve
 })
 
 
-// section-4----->
+
+
 document.getElementById('btn-donate-04').addEventListener('click', function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const addDonation = getInputFieldValueById('input-field-value-04');
     const accountBalance = getTextValueById('account-balance');
@@ -132,17 +127,17 @@ document.getElementById('btn-donate-04').addEventListener('click', function (eve
 
     if ((addDonation > 0) && (typeof parseFloat(addDonation) === 'number')) {
 
-        if(parseFloat(addDonation) > parseFloat(accountBalance)){
+        if (parseFloat(addDonation) > parseFloat(accountBalance)) {
             alert('Invalid Donation Amount');
             return;
         }
         const updateDonation = parseFloat(addDonation) + parseFloat(oldDonation);
         document.getElementById('old-donation-balance-04').innerText = updateDonation;
         const newPageBalance = parseFloat(accountBalance) - parseFloat(addDonation);
-        document.getElementById('account-balance').innerText = newPageBalance
+        document.getElementById('account-balance').innerText = newPageBalance;
 
-        const currentTime = new Date()
-        const div = document.createElement('div')
+        const currentTime = new Date();
+        const div = document.createElement('div');
         div.innerHTML = `
             <div class="border  bg-[#F9F7F3] border-gray-200 rounded-lg p-8 mt-12">
                 <p class=" text-lg font-bold ">${addDonation} Taka is ${floodArea}</p>
@@ -150,11 +145,8 @@ document.getElementById('btn-donate-04').addEventListener('click', function (eve
             </div>
             `
         document.getElementById('history-container').appendChild(div);
-     
     }
-
     else {
         alert('Invalid Donation Amount');
     }
- 
 })
--- js/features.js
+++ js/features.js
@@ -2,6 +2,7 @@
 
 document.getElementById('btn-history').addEventListener('click', function(){
 
+    document.getElementById('btn-donation').classList.remove('active-donation');
     document.getElementById('history-container').classList.remove('hidden');
     document.getElementById('donation-container').classList.add('hidden');
 
